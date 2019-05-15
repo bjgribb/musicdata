2066d5b201d4341081dfc0b5dbdfd674db68bc1c
bjgribb <bjgribb@gmail.com>
2019-05-15 14:42:51 -0400

    setting up index

--- spotify_python/views.py
+++ spotify_python/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+import spotipy
+
+
+def index(request):
+
+    spotify = spotipy.Spotify()
+    results = spotify.search(q='artist: gideon', type='artist')
+
+    context = {
+        'results': results,
+    }
+
+    return render(request, 'index.html', context=context)
