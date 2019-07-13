d3f20c039f2151c39d97b9b41377cf2b18a5bcd1
bjgribb <bjgribb@gmail.com>
2019-07-13 11:03:09 -0400

    updated modals for track meta-data using bootstrap

--- spotify_python/static/js/main.js
+++ spotify_python/static/js/main.js
@@ -139,26 +139,15 @@ function getTrackInfo (token, trackId) {
     },
     success: function (response) {
       playerInfo.innerHTML =
-      `<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-          Launch demo modal
+      `<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#danceabilityModal">
+          Danceability: ${response.danceability}
         </button>
-    
-      <!-- Modal -->
-      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              ...
-            </div>
-          </div>
-        </div>
-      </div>`
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#energyModal">
+          Energy: ${response.energy}
+        </button>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#acousticModal">
+          Acousticness: ${response.acousticness}
+        </button>`
     }
   })
 }
